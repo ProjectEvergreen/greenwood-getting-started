dada165a50c84aeec69511e3516c3efa72b3ca90
aholtzman <adam.holtzman@gmail.com>
2020-10-04 11:09:22 -0400

    add meta tag with description for SEO requirements

--- greenwood.config.js
+++ greenwood.config.js
@@ -1,3 +1,8 @@
+const pkg = require('./package.json');
+
 module.exports = {
-  title: 'My Personal Blog'
+  title: 'My Sizzle Blog',
+  meta: [
+    { name: 'description', content: pkg.description } // replace the pkg.description with a custom string if you prefer
+  ]
 };
